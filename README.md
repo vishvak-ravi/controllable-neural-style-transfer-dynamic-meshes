da3f5632f66b70b2565126e698a37732a7e7c431
vish <vishravi03@gmail.com>
2025-05-19 13:31:40 -0400

    add examples and updated readme

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # Controllable Neural Style Transfer for Dynamic Meshes
 
-Here's an implementation of [Controllable Neural Style Transfer for Dynamic Meshes](https://studios.disneyresearch.com/2024/07/28/controllable-neural-style-transfer-for-dynamic-meshes/) by Haetinger et al. Note that this only supports static meshes.
+Here's an implementation of [Controllable Neural Style Transfer for Dynamic Meshes](https://studios.disneyresearch.com/2024/07/28/controllable-neural-style-transfer-for-dynamic-meshes/) by Haetinger et al. Note that this only supports static meshes without colorization (for now).
 
-TL;DR—this jitters 3D vertices to minimize the distance between features of a reference style image and ones extracted from images produced by a differentiable renderer.
+<p align="center">
+    <img src="examples/spots.png" alt="hi its me" />
+</p>
+<p align="center">Original version of <a href=https://www.cs.cmu.edu/~kmcrane/Projects/ModelRepository>Spot</a> on the left with styles appearing in their respective order.</p>
+
+TL;DR—this jitters 3D vertices to minimize the MSE between early layer activations of VGG16 from reference style image and ones extracted from images produced by a differentiable renderer.
 
 ## Usage
 1. Clone repo and place an .obj file to ```data/``` and a style image in ```data/styles``` (anywhere really is fine)
@@ -10,10 +15,30 @@ TL;DR—this jitters 3D vertices to minimize the distance between features of a 
 ```bash
 python model/transfer.py
 ```
-3. (Optional) finetune the learning rate, number of iterations, smoothness factors, and masking ratios as needed. The original paper offers starting points for these.
+3. (Optional) finetune the learning rate, number of iterations per smoothness level, smoothness factors, and masking ratios as needed. The original paper offers starting points for these.
 
 ## Implementation Notes
-- I used a compute cluster with 1x A100 40Gb with 200Gb RAM. The original paper used an RTX 3090 with an unknown amount of RAM.
-- ```spot_280k.obj``` with the given parameters takes ~4 minutes to optimize, but takes the authors 2, so hyperparameter selection likely explains this. 
-- Poisson sampling in 3D is not trivial when trying to sample along the surface of a sphere. Instead, since the elevation range is small, directly poisson sampling azimuth and elevation approximates blue noise on a sphere's surface.
-- The Cholesky decomposition of the Laplace-Beltrami of the mesh runs on CPU since it is too large for GPU nor are sparse Cholesky operations supported by ```pytorch```. Instead, a custom backwards operation is used in ```model/cholesky.py``` leveraging ```scikit-sparse```
+- I used a compute cluster with 1x A100 40Gb. The original paper used an RTX 3090.
+- ```spot_280k.obj``` with the given parameters takes ~4 minutes to optimize, but takes the authors 2 minutes, so hyperparameter selection + setup difference likely explains this. 
+- Poisson sampling is not *truly* Poisson
+- Cholesky factoring and solving uses ```scikit-sparse``` with a custom backwards pass for the latter, but the lower triangular factor is not guaranteed to be sparse. Still, a dense Laplacian-Beltrami matrix for a 200k vertex mesh at fp32 is over 120 Gb!
+
+<p align="center">
+    <img src="examples/merlions.png" alt="hi its me" />
+</p>
+<p align="center"><a href=https://skfb.ly/6tNNG>Original merlion</a></p>
+
+### Dependencies
+I'll make a ```requirements.txt``` later, but for now...
+- Pytorch
+- Pytorch3D
+- scipy
+- scikit-sparse
+
+### Future TODOs
+
+I think a few READMEs say "I'll come back to it" but especially this one has a bit more work to do to make it anywhere close to the outputs on the paper.
+
+- Debug to pipeline to smoothen meshes (outputs are VERY wrinkly) and better transfer style at lower Laplacian coefficients
+- Add support for dynamic meshes
+- Add support for optimizing vertex colors
--- model/transfer.py
+++ model/transfer.py
@@ -16,7 +16,6 @@ from style_utils import (
     get_combinatorial_laplacian,
     cholesky_factor,
     CholeskyFactorRoutine,
-    LaplacianRoutine,
 )
 from rendering_utils import (
     render_mono_texture_from_meshes,
@@ -24,20 +23,20 @@ from rendering_utils import (
 )
 import PIL
 
-N_ITERS = torch.tensor([30, 30, 50])
-LAMBDAS = torch.tensor([20, 5, 1])
+N_ITERS = torch.tensor([50, 40, 30])
+LAMBDAS = torch.tensor([20, 5, 0.5])
 MASK_RATIOS = torch.tensor([0.2, 0.1, 0])
-LR = torch.tensor([8.0e-3, 2.0e-3, 0.5e-3])
+LR = torch.tensor([10.0e-3, 3.0e-3, 1e-3])
 BLUR_RADII = torch.tensor([0, 0, 0])
-IMAGE_SCALES = torch.tensor([0.4, 0.8, 1.5])
-RESOLUTION = 1000
+IMAGE_SCALES = torch.tensor([0.5, 1.0, 1.5])
+RESOLUTION = 700
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
 def transfer_style(style_reference_path, input_mesh_path, cfg: dict = {}):
 
-    batch_size = cfg.get("batch_size", 3)
+    batch_size = cfg.get("batch_size", 4)
     num_rots = cfg.get("num_rotations", 8)
     writer = SummaryWriter()
     global_step = 0
@@ -56,13 +55,11 @@ def transfer_style(style_reference_path, input_mesh_path, cfg: dict = {}):
         ref_style_features = unnormalized_ref_style_features - torch.mean(
             unnormalized_ref_style_features, dim=(2, 3), keepdim=True
         )
-        
+
         # get mesh and its laplacian
         orig_mesh, verts = vertex_preprocess_from_mesh_path(input_mesh_path)
         print("getting laplacian")
-        laplace_beltrami = get_combinatorial_laplacian(
-            orig_mesh, LaplacianRoutine.CUSTOM, log_time=True
-        )
+        laplace_beltrami = get_combinatorial_laplacian(orig_mesh, log_time=True)
 
         V = verts.size(0)
         x_hat = verts.clone()
@@ -72,7 +69,9 @@ def transfer_style(style_reference_path, input_mesh_path, cfg: dict = {}):
         LAMBDAS, MASK_RATIOS, LR, N_ITERS, BLUR_RADII, IMAGE_SCALES
     ):
         with torch.no_grad():
-            mask = (torch.rand(V, device=device) < mask_ratio).to(device) # volume regularization mask
+            mask = (torch.rand(V, device=device) < mask_ratio).to(
+                device
+            )  # volume regularization mask
             print("computed mask")
 
             A = laplace_beltrami.clone() * lam  # compute A = I * lambda + Laplace
@@ -102,26 +101,30 @@ def transfer_style(style_reference_path, input_mesh_path, cfg: dict = {}):
             opt.zero_grad()
             t0 = time.time()
             print("solving cholesky")
-            x_hat = cholmod_solve(x_star, L) # get vertices from implicit paramterization
+            x_hat = cholmod_solve(
+                x_star, L
+            )  # get vertices from implicit paramterization
             x_hat = x_hat.to(device)
             print(f"solved cholesky in {time.time() - t0} s")
 
-            imgs: torch.Tensor = render_mono_texture_from_meshes( # construct and render mesh
-                Meshes(
-                    [x_hat for _ in range(batch_size)],
-                    faces=[orig_mesh.faces_list()[0] for _ in range(batch_size)],
-                ),
-                batch_size=batch_size,
-                poisson_radius=0.20,
-                blur_radius=blur_radius,
-                resolution=RESOLUTION * img_scale,
-                save_name=f"data/renders/{lam}_{i}",
+            imgs: torch.Tensor = (
+                render_mono_texture_from_meshes(  # construct and render mesh
+                    Meshes(
+                        [x_hat for _ in range(batch_size)],
+                        faces=[orig_mesh.faces_list()[0] for _ in range(batch_size)],
+                    ),
+                    batch_size=batch_size,
+                    poisson_radius=0.20,
+                    blur_radius=blur_radius,
+                    resolution=RESOLUTION * img_scale,
+                    save_name=f"data/renders/{lam}_{i}",
+                )
             )
             print("rendered!")
 
             imgs = imgs.permute(0, 3, 1, 2)
             feats = style_extractor(render_transform(imgs))
-            
+
             # normalize content features with running average
             with torch.no_grad():
                 if global_step == 0:
@@ -135,7 +138,7 @@ def transfer_style(style_reference_path, input_mesh_path, cfg: dict = {}):
                     ) / feature_count
 
             normalized_feats = feats - mean_content_features
-            
+
             # compute MSE between feats and its nearest neighbor from the reference style feats
             repl = nearest_neighbor_replacement(
                 ref_style_features, normalized_feats, tau=1e10
@@ -147,7 +150,7 @@ def transfer_style(style_reference_path, input_mesh_path, cfg: dict = {}):
                 writer.add_scalar("loss", loss.item(), global_step)
                 writer.add_scalar("grad_norm", x_star.grad.norm().item(), global_step)
 
-            x_star.grad[mask] = 0 # use the volume regularization mask
+            x_star.grad[mask] = 0  # use the volume regularization mask
             opt.step()
             global_step += 1
             print(f"Step {i} took: {time.time() - t_start}")
@@ -164,11 +167,11 @@ def transfer_style(style_reference_path, input_mesh_path, cfg: dict = {}):
 
 
 if __name__ == "__main__":
-    style_img = "data/styles/starry.png"
+    style_img = "data/styles/triangles.png"
     obj = "data/spot_210k.obj"
     output_mesh: Meshes = transfer_style(style_img, obj)
     save_obj(
-        f"data/mesh_outputs/spot_starry.obj",
+        f"data/mesh_outputs/spot_triangles.obj",
         output_mesh.verts_list()[0],
         output_mesh.faces_list()[0],
     )
